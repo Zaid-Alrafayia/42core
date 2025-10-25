b27da6524540c64c82ba2aa61bdadb05066b7d27
zaid-alrafayia <zaidomar678@gmail.com>
2025-10-25 18:25:06 +0300

    ft_printf init

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaalrafa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 15:23:53 by zaalrafa          #+#    #+#             */
+/*   Updated: 2025/10/25 17:12:56 by zaalrafa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "libft/libft.h"
+
+int	ft_printf(const char *str, ...);
