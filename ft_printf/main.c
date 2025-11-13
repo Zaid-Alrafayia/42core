c4964f56d436b4a168e62647728a4fda6c944b98
zaid-alrafayia <zaidomar678@gmail.com>
2025-11-13 13:24:25 +0300

    delete v0

--- ft_printf/main.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-
-int	main(void)
-{
-	ft_printf("printf  : char:%c \nstring:%s \nptr:%p \ndec:%d \nint:%i\nunsigned:%u \nhex_lower:%x \nhex_upper:%X \npercent: %%\n", 'a', "hello my name is zaid", (void *)0xDEADBEEF,-12345, 12345, 4294967295u, 305441741u, /* 0x1234ABCD */ 305441741u);
-	return (0);
-}
