52d1d7dda95111ab4e876113b6f9aef8d7579320
zaid-alrafayia <zaidomar678@gmail.com>
2025-12-30 21:30:40 +0300

    pipex mandatory

--- pipexV.1/pipex.c
+++ pipexV.1/pipex.c
@@ -6,29 +6,43 @@
 /*   By: zaalrafa <zaalrafa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 20:10:25 by zaalrafa          #+#    #+#             */
-/*   Updated: 2025/12/30 21:07:12 by zaalrafa         ###   ########.fr       */
+/*   Updated: 2025/12/30 21:28:27 by zaalrafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pipex.h"
+#include <sys/wait.h>
 #include <unistd.h>
 
 void	pipex(t_pipex *px)
 {
-	char	*cmd_path1;
-	char	*cmd_path2;
-	char	**split_cmd1;
-	char	**split_cmd2;
+	int	pid1;
+	int	pid2;
 
-	split_cmd1 = cmd_split(px->argv[2]);
-	split_cmd2 = cmd_split(px->argv[3]);
-	cmd_path1 = check_path(&px, split_cmd1[0]);
-	cmd_path2 = check_path(&px, split_cmd2[0]);
+	if (pipe(px->fd) == -1)
+		error_exit("pipex", 2);
+	pid1 = fork();
+	if (pid1 == 0)
+		child_process(px, 2);
+	pid2 = fork();
+	if (pid2 == 0)
+		child_process(px, 3);
+	close(px->fd[0]);
+	close(px->fd[1]);
+	close(px->outfd);
+	close(px->infd);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 }
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_pipex	px;
 
+	if (argc != 5)
+	{
+		ft_putendl_fd("Usage: ./pipex infile cmd1 cmd2 outfile", 2);
+		return (1);
+	}
 	px.argc = argc;
 	px.argv = argv;
 	px.envp = envp;
